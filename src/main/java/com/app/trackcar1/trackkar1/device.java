e45ec14f9b13419088b6bea6da7fbb8e77106b1b
mlipski <mariusz.lipski@gmail.com>
2020-04-07 08:37:58 +0200

    add class device

---
+++ src/main/java/com/app/trackcar1/trackkar1/device.java
@@ -0,0 +1,34 @@
+package com.app.trackcar1.trackkar1;
+
+public class device {
+
+    private Long id;
+
+    private  String name;
+
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
