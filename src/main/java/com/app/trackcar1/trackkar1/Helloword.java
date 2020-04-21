cee5eef0191a84da28f1fba8fc441793810a76de
mlipski <mariusz.lipski@gmail.com>
2020-04-21 11:13:03 +0200

    try api

--- src/main/java/com/app/trackcar1/trackkar1/Helloword.java
+++ src/main/java/com/app/trackcar1/trackkar1/Helloword.java
@@ -1,24 +1,14 @@
 package com.app.trackcar1.trackkar1;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@Service
-@Component
-@Repository
-@Controller
 @RestController
 public class Helloword {
 
-        @RequestMapping("/")
-        public String index(){
-            return "Api";
+        @GetMapping("/Hello")
+        public String Hello(){
+            return "Apiiiss";
         }
 
 }
